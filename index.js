7fc2265d6acd138e0425ed4b3cab60cd30e2a563
Christian Robbers Rasmussen <crr@likehero.dk>
2023-11-09 14:11:40 +0100

    added some html and javescript

---
+++ index.js
@@ -0,0 +1,23 @@
+const baseUrl = "https://musicrecordsrest20231109134019.azurewebsites.net/api/musicrecords"
+
+Vue.createApp({
+    data() {
+        return {
+            musicrecords: [],
+            error: "der skete en fejl mongol"
+        }
+    },
+    methods: {
+        async getMusicRecords(baseUrl) {
+            try {
+
+                const response = await axios.get(baseUrl)
+                this.musicrecords = await response.data
+            } catch(error) {
+
+                this.error = error.message
+
+            }
+        }
+    }
+}).mount('#app')
